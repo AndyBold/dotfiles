0d7e540023e14e372977e127d8c974c8a5c3a7d4
Andy Bold <abold@launchdarkly.com>
2020-08-02 18:23:55 +0100

    More tweaks

---
+++ dotfiles/fish/dotconfig/conf.d/virtualfish-loader.fish
@@ -0,0 +1,6 @@
+set -g VIRTUALFISH_VERSION 2.4.0
+set -g VIRTUALFISH_PYTHON_EXEC /Users/andyb/.pyenv/versions/3.8.2/bin/python3.8
+set -g VIRTUALFISH_HOME /Users/andyb/src/virtualenv
+source /Users/andyb/.pyenv/versions/3.8.2/lib/python3.8/site-packages/virtualfish/virtual.fish
+source /Users/andyb/.pyenv/versions/3.8.2/lib/python3.8/site-packages/virtualfish/compat_aliases.fish
+emit virtualfish_did_setup_plugins
